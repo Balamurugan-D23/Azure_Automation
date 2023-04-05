9e1743d90e11ae4aea5f7c46ff988a9ac55554a9
balam <balam@BatMan>
2023-04-05 19:12:13 +0530

    First Commit

---
+++ Automation_azure/src/main/java/automation/Sample.java
@@ -0,0 +1,17 @@
+package automation;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+	public void Test1() {
+		System.out.println("Test 1 is Executed..");
+	}
+
+	@Test
+	public void Test2() {
+		System.out.println("Test 2 is Executed..");
+	}
+
+}
