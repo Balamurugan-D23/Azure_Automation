ab67476d47b7dd0872434e2935b31f08b52e58d6
balam <balam@BatMan>
2023-04-05 22:10:58 +0530

    Adding test class

---
+++ Azure_Automation/src/main/java/test/TestAutomation.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class TestAutomation {
+
+	@Test
+	public void Test1() {
+		System.out.println("Test 1 Executed");
+	}
+
+	@Test
+	public void Test2() {
+		System.out.println("Test 2 Executed");
+	}
+
+	@Test
+	public void Test3() {
+		System.out.println("Test 3 Executed");
+	}
+}
