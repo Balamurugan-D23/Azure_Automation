07634c18e7c7e98a69b6cbcab05498703206593b
balam <balam@BatMan>
2023-04-09 13:28:25 +0530

    Selenium Update

--- Azure_Automation/src/main/java/test/TestAutomation.java
+++ Azure_Automation/src/main/java/test/TestAutomation.java
@@ -1,6 +1,14 @@
 package test;
 
+import static org.testng.Assert.assertTrue;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class TestAutomation {
 
@@ -18,6 +26,27 @@ public class TestAutomation {
 	public void Test3() {
 		System.out.println("Test 3 Executed");
 	}
-	
-	
+
+	@Test
+	public void Test4() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.google.com/");
+
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (driver.getTitle().equals("Google")) {
+			System.out.println("PASS");
+			assertTrue(true);
+		} else {
+			System.out.println("FAIL");
+			assertTrue(false);
+		}
+	}
+
 }
